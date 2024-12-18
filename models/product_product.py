0573cc9929ba8cda1fa4b244e3d61912b14d6e42
Roberto-RRF <69941000+Roberto-RRF@users.noreply.github.com>
2024-12-18 16:26:27 -0600

    Update product_product.py

--- models/product_product.py
+++ models/product_product.py
@@ -9,32 +9,38 @@ class ProductProduct(models.Model):
     @api.depends('qty_available')
     def _compute_millares_rollos_on_hand(self):
         for record in self:
-            if record.product_cosal == 'hoja':
-                for uom in record.secondary_uom_ids:
-                    if uom:
-                        record.millares_rollos_a_la_mano = record.qty_available / uom.factor_inv
-                        break
-                    else:
-                        record.millares_rollos_a_la_mano = None
-            elif record.product_cosal == 'rollo':
-                res = self.env['stock.lot'].search([('product_id','=',record.id)])
-                record.millares_rollos_a_la_mano = len(res)
-            else:
-                record.millares_rollos_a_la_mano = ""
+            try:
+                if record.product_cosal == 'hoja' and record.secondary_uom_ids:
+                    for uom in record.secondary_uom_ids:
+                        if uom:
+                            record.millares_rollos_a_la_mano = record.qty_available / uom.factor_inv
+                            break
+                        else:
+                            record.millares_rollos_a_la_mano = None
+                elif record.product_cosal == 'rollo' and record.secondary_uom_ids:
+                    res = self.env['stock.lot'].search([('product_id','=',record.id)])
+                    record.millares_rollos_a_la_mano = len(res)
+                else:
+                    record.millares_rollos_a_la_mano = None
+            except:
+                record.millares_rollos_a_la_mano = None
 
     @api.depends('free_qty')
     def _compute_millares_rollos_disponible(self):
         for record in self:
-            if record.product_cosal == 'hoja':
-                for uom in record.secondary_uom_ids:
-                    if uom:
-                        record.millares_rollos_disponible = record.free_qty / uom.factor_inv
-                        break
-                    else:
-                        record.millares_rollos_disponible = None
-            elif record.product_cosal == 'rollo':
-                res = self.env['stock.lot'].search([('product_id','=',record.id),('product_qty','>',0)])
-                record.millares_rollos_disponible = len(res)
-            else:
+            try: 
+                if record.product_cosal == 'hoja':
+                    for uom in record.secondary_uom_ids:
+                        if uom:
+                            record.millares_rollos_disponible = record.free_qty / uom.factor_inv
+                            break
+                        else:
+                            record.millares_rollos_disponible = None
+                elif record.product_cosal == 'rollo':
+                    res = self.env['stock.lot'].search([('product_id','=',record.id),('product_qty','>',0)])
+                    record.millares_rollos_disponible = len(res)
+                else:
+                    record.millares_rollos_disponible = None
+            except:
                 record.millares_rollos_disponible = None
         
